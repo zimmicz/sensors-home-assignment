0513e95c5c193de4923444f2fb3bcbc5274824c0
Michal Zimmermann <zimmicz@gmail.com>
2023-07-28 11:30:37 +0200

    chore: drop vite logo

--- src/app.tsx
+++ src/app.tsx
@@ -5,11 +5,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
